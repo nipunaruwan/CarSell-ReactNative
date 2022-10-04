49a9eb0200e20c4ab6b504260441ebe8b5d623b5
mipunaruwan <nipunakalpa1234@gmail.com>
2022-10-04 23:12:39 +0530

    package are created

---
+++ App.js
@@ -0,0 +1,18 @@
+import { View, Text,Button } from 'react-native'
+import React from 'react'
+import Login from './Screens/Login';
+
+
+
+export default function App() {
+  return (
+  
+
+    <View>
+      
+
+   <Login/>
+    </View>
+   
+  );
+}
---
+++ Screens/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View,Text,Button } from "react-native";
+
+export default function Login(){
+
+    return(
+       
+    <View>
+    <Text style={{color:'blue' ,fontSize:20}}>login page</Text>
+    <Text style={{color:'red'}}>Maincomponent</Text>
+   
+   <Button title="click"  color="blue" /> 
+ 
+ 
+  </View> 
+
+    );
+}
